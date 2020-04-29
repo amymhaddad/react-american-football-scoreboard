b81fe2bd62f67d56259f0a9b27ced10cfa8f77e5
Amy M Haddad <amy.m.haddad@gmail.com>
2020-04-29 07:19:13 -0400

    Final changes to app

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,31 +1,19 @@
 import React, { useState } from "react";
-import ScoreHome from "./ScoreHome";
-import ScoreAway from "./ScoreAway";
 import BottomRow from "./BottomRow";
 import TeamScore from "./TeamScore";
 
-
-//make a general component called TeamScore compoentnt that I will use 2x -- one for home and one for away -- and pass in the correct data
-{/* <TeamScore className="home__score" teamName = 'Home' teamScore = {homeScore} setScore={setHomeScore}/>   */}
-//Define it once but call it twice. So I have a single JS file and I'll render it 2x
 export default function ScoreBoard() {
     const [homeScore, setHomeScore] = useState(0);
     const [awayScore, setAwayScore] = useState(0);
-
-
 
     return (
 
         <div className="container">
           <section className="scoreboard">
             <div className="topRow">
-
               <TeamScore className="home__score" teamName="Lions" score={homeScore} setScore={setHomeScore} />
-
-              {/* <ScoreHome className="home__score" homeScore = {homeScore} setHomeScore={setHomeScore}/>   */}
-              
               <div className="timer">00:03</div>
-              <ScoreAway className="away_score" awayScore={awayScore} setAwayScore={setAwayScore}/>
+              <TeamScore className="away__score" teamName="Tigers" score={awayScore} setScore={setAwayScore} />
           </div>
           <BottomRow />
         </section>
--- src/components/TeamScore.js
+++ src/components/TeamScore.js
@@ -4,32 +4,25 @@ export default function TeamScore({className, teamName, score, setScore}) {
     const touchDown = 7;
     const fieldGoal = 3;
 
-
     return (
-
         <div className="container">
-        <div className="home">
-            <h2 className="home__name">{teamName}s</h2>
-            <div className={className}>{score}</div>
-        </div>
-
-        <section className="buttons">
-            <div className="homeButtons">
-                <button 
-                    className="homeButtons__touchdown"
-                    onClick= {() => setScore(score + touchDown) }
-                    >Home Touchdown</button>
-                <button 
-                    className="homeButtons__fieldGoal"
-                    onClick={() => setScore(score + fieldGoal)}
-                    >Home Field Goal</button>
+            <div className="home">
+                <h2 className="home__name">{teamName}</h2>
+                <div className={className}>{score}</div>
             </div>
-        </section>
+
+            <section className="buttons">
+                <div className="homeButtons">
+                    <button 
+                        className="homeButtons__touchdown"
+                        onClick= {() => setScore(score + touchDown) }
+                        >Home Touchdown</button>
+                    <button 
+                        className="homeButtons__fieldGoal"
+                        onClick={() => setScore(score + fieldGoal)}
+                        >Home Field Goal</button>
+                </div>
+            </section>
     </div>
-
-
-
     )
-
-
 }
